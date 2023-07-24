b85c63160d250063c65fef86e5d5702f21eb2e6f
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-07-24 17:21:46 -0400

    Added link to home

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
 import Logo from "../assets/logo_no_text.svg";
 import styles from "../styles/header.module.css";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image
-          src={Logo}
-          alt="MammaCare Logo"
-          height={48}
-          className={styles.logoImg}
-        />
-        <h1 className={styles.logoText}>
-          MammaCare<sup className={styles.reg}>&reg;</sup>
-        </h1>
-      </div>
-      <h2 className={styles.tagline}>
-        Advancing breast exam standards worldwide since 1974
-      </h2>
+      <Link href="/" style={{ textDecoration: "none" }}>
+        <div className={styles.logo}>
+          <Image
+            src={Logo}
+            alt="MammaCare Logo"
+            height={48}
+            className={styles.logoImg}
+          />
+          <h1 className={styles.logoText}>
+            MammaCare<sup className={styles.reg}>&reg;</sup>
+          </h1>
+        </div>
+        <h2 className={styles.tagline}>
+          Advancing breast exam standards worldwide since 1974
+        </h2>
+      </Link>
     </header>
   );
 };
