76c002e704d45d346833e6c18c3a94cb6467faa9
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-07-26 12:19:54 -0400

    Added shortcut to studio

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
+import React from "react";
+import { useRouter } from "next/router";
 import Layout from "../components/layout";
 import "../styles/styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    document.addEventListener(
+      "keydown",
+      ({ repeat, metaKey, ctrlKey, altKey, key }) => {
+        if (repeat) return;
+        if ((metaKey || ctrlKey) && altKey && key === "s") {
+          router.push(`${router.basePath}/api/preview`);
+        }
+      }
+    );
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
